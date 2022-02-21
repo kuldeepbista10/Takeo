1daa02b5e1253d0aa49f195c8410764dd654d5cd
kuldeepbista10 <kuldeepbista10@gmail.com>
2022-02-20 19:23:21 -0800

    css and javascript files added

---
+++ app.js
@@ -0,0 +1,9 @@
+// alert("hello");
+// console.log("hi how are you?")
+console.dir(document);
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.URL);
+console.log(document.getElementById("root"));
+console.log(document.getElementById("root").textContent);
